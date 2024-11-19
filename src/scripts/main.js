a3b81b2f87f6c255f69af0e8315a52e8a72c20da
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2024-11-19 12:31:57 -0500

    PotteryList.js added, as well as .innerHTML that returns HTML of sellable items along with relavent details.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@
 import { makePottery } from './PotteryWheel.js'
 import { firePottery } from './Kiln.js'
 import { toSellOrNotToSell, usePottery } from './PotteryCatalog.js'
+import { PotteryList } from './PotteryList.js'
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery('Mug', 1.5, 8)
@@ -30,6 +31,14 @@ plate = toSellOrNotToSell(plate)
 
 //console.log(usePottery())
 
-
 // Invoke the component function that renders the HTML list
- 
+debugger
+ const potteryHTML = PotteryList(usePottery())
+ let potteryInnerHTML = document.getElementsByClassName("potteryList")[0] //used index of [0] to access the element due to getElementsByClassName syntax. It needs to know what to target. 
+potteryInnerHTML.innerHTML = potteryHTML
+
+ /*if (potteryInnerHTML){
+    potteryInnerHTML = potteryHTML;
+ }else {
+    console.error('Could not find element with the class "potteryList')
+ }*/
